f0dae1e2117b54f304cc0595ed8b2c1de2a7cfcb
davon <747664918@qq.com>
2023-08-16 22:16:29 +0800

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,58 +1,61 @@
-import React, { useState } from 'react';
-import ReactPaginate from 'react-paginate';
+import React, {useState} from 'react';
+import axios from 'axios';
+
 
 import './App.css';
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [currentPage, setCurrentPage] = useState(1); // 当前页码
-  const resultsPerPage = 5; // 每页显示的结果数量
+    const [searchTerm, setSearchTerm] = useState('');
 
-  const mockSearchResults = [
-    { title: 'Search Result 1', content: 'This is the content of the first search result.' },
-    { title: 'Search Result 2', content: 'This is the content of the second search result.' },
-    // ...添加更多结果...
-  ];
+    const mockSearchResults = [
+        {title: 'Search Result 1', content: 'This is the content of the first search result.'},
+        {title: 'Search Result 2', content: 'This is the content of the second search result.'},
+        // ...添加更多结果...
+    ];
 
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
+    const handleSearchChange = (event) => {
+        console.log("aaa")
+        setSearchTerm(event.target.value);
 
-  const handlePageChange = (selectedPage) => {
-    setCurrentPage(selectedPage);
-  };
+        console.log("dddd")
+        // Send POST request with searchTerm to backend
+        axios.get('http://192.168.0.110:5000/search').then(response => {
+                console.log(response.data);
+                // Handle the response data as needed
+            })
+            .catch(error => {
+                console.error(error);
+                // Handle error
+            });
+    };
 
-  const startIndex = (currentPage - 1) * resultsPerPage;
-  const endIndex = startIndex + resultsPerPage;
-  const displayedResults = mockSearchResults.slice(startIndex, endIndex);
-
-  return (
-      <div className="App">
-        <header className="App-header">
-          <input
-              type="text"
-              value={searchQuery}
-              onChange={handleSearchChange}
-              placeholder="Search Google"
-              className="search-box"
-          />
-          {displayedResults.map((result, index) => (
-              <div key={index} className="search-result">
-                <h2>{result.title}</h2>
-                <p>{result.content}</p>
-              </div>
-          ))}
-          <div className="pagination">
-            <ReactPaginate
-                pageCount={Math.ceil(mockSearchResults.length / resultsPerPage)}
-                onPageChange={({ selected }) => handlePageChange(selected + 1)}
-                containerClassName="pagination"
-                activeClassName="active"
-            />
-          </div>
-        </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    placeholder="Search"
+                    className="search-box"
+                />
+                {mockSearchResults.map((result, index) => (
+                    <div key={index} className="search-result">
+                        <h2>{result.title}</h2>
+                        <p>{result.content}</p>
+                    </div>
+                ))}
+                {/*<div className="pagination">*/}
+                {/*  <ReactPaginate*/}
+                {/*      pageCount={Math.ceil(mockSearchResults.length / resultsPerPage)}*/}
+                {/*      onPageChange={({ selected }) => handlePageChange(selected + 1)}*/}
+                {/*      containerClassName="pagination"*/}
+                {/*      activeClassName="active"*/}
+                {/*  />*/}
+                {/*</div>*/}
+            </header>
+        </div>
+    );
 }
 
 export default App;
